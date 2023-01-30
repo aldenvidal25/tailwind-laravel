49fe8e41f93c382bf7ea3ea7f4c72f1209b2b692
Alden Vidal <aldenvidal25@gmail.com>
2023-01-30 14:33:11 +0800

    email

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -9,6 +9,14 @@
 <main class="bg-white max-w-lg mx-auto p-8 my-10 rounded-lg shadow-2x1">
     <section>
         <h3 class="font-bold text-2x1 text-center">Welcome To Student System</h3>
+        <p class="text-fray-600 pt-2">Sign in to your account</p>
+    </section>
+    <section>
+        <form action="" class="flex flex-col">
+            <div class="mb6 pt-3 rounded bg-gray-200">
+                <label for="email" class="block text-gray-700 text-sm font-bold mb-2 ml-3">Email</label>
+            </div>
+        </form>
     </section>
 
 @include('partials.footer')
