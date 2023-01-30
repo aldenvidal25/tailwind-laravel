7e697474a069c759d79a44f23086544424bbf542
Alden Vidal <aldenvidal25@gmail.com>
2023-01-30 17:37:53 +0800

    refactor navbar/ components

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,23 +1,6 @@
 @include('partials.header')
-<nav x-data="{ open: false }" class="bg-gray-800 fixed w-full z-20 top-0 left-0 border-gray-200 px-2 sm:px-4 py-2.5 text-white">
-    <div class="container flex flex-wrap justify-between items-center">
-        <a href="/">
-            <span class="self-center text-xl font-semibold whitespace-nowrap">
-                Student System
-            </span>
-        </a>
-        <button @click="open = !open" data-collapse-toggle="navbar-main" class="md:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M6 36v-3h36v3Zm0-10.5v-3h36v3ZM6 15v-3h36v3Z"/></svg>
-        </button>
-        <div x-show="open" class="w-full md:block md:w-auto" id="navbar-main">
-            <ul class="flex flex-col md:flex-row px-4">
-                <li><a href="#" class="block py-2 pr-4 pl-3">Sign In</a></li>
-                    <li> <a href="#" class="block py-2 pr-4 pl-3">Sign Up</a></li>
-                    <li> <a href="#" class="block py-2 pr-4 pl-3">Logout</a></li>           
-            </ul>
-        </div>
-    </div>
-</nav>
+<?php $array = array('title' => 'Student System Ko Ito') ?>
+<x-nav :data="$array" />
 
 <header class="max-w-lg mx-auto mt-5">
 <a href="#">
